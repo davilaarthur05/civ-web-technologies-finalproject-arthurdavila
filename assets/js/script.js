cadee3639c0efc855da4e8847da8f30037afee77
Arthur <davila.arthur05@gmail.com>
2024-06-20 14:48:02 +0800

    fix: second JS implement

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,31 @@
 const menuTrigger = document.getElementById("menu-icon")
 const menu = document.getElementById("nav-menu");
+const items = document.querySelectorAll(".carousel-image");
+let currentIndex = 0;
 
 menuTrigger.addEventListener('click', ()=>{
     menu.classList.toggle("top-[70px]");
 });
+
+function showSlide(index) {
+    items.forEach((item, i) => {
+        if (i === index) {
+            item.classList.add('active');
+        } else {
+            item.classList.remove('active');
+        }
+    });
+    document.querySelector(".carousel").style.transform = `translateX(-${index * 100}%)`;
+}
+
+function prevImage() {
+    currentIndex = (currentIndex > 0) ? currentIndex - 1 : items.length - 1;
+    showSlide(currentIndex);
+}
+
+function nextImage() {
+    currentIndex = (currentIndex < items.length - 1) ? currentIndex + 1 : 0;
+    showSlide(currentIndex);
+}
+
+showSlide(currentIndex);
